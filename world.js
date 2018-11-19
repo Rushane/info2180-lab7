8845acfbba6baf46bccef72c4ebd7ca26016a821
Rushane Barnes <barnesrushane@hotmail.com>
2018-11-19 17:44:41 +0000

    Exercise 4

--- world.js
+++ world.js
@@ -1,18 +1,36 @@
-function lookUp () {   
+function lookUp () { 
+    let url;
     let country =document.getElementById("country").value;
+    let all= document.getElementById('myCheck');
     
     let httpRequest = new XMLHttpRequest();
     
     httpRequest.onreadystatechange= function(){
         if(this.readyState==4 && this.status==200){
-            alert(this.responseText);
-            //document.getElementById("result").innerHTML=this.responseText;
+            document.getElementById("result").innerHTML=this.responseText;
+            //handleResponse(this.responseText);
+            
         }
     };
-    httpRequest.open("GET","world.php?country=" + country,true);
+    
+    if (all.checked)
+    {
+          all.setAttribute('value','true');
+          url = "world.php?country="+"&all="+ all.value;
+          
+    }
+    else {
+        url =  "world.php?country=" + country;
+    }
+    
+    httpRequest.open("GET",url,true);
     httpRequest.send();
     
     //});
+    function handleResponse (response) {
+         alert(response);
+    }
 }
+
 
 
--- world.php
+++ world.php
@@ -1,19 +1,30 @@
 <?php
 
 $country=$_GET['country'];
+$all =$_GET['all'];
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
-
 //global $statement;
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$statement = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
- echo '<ul>';
-foreach ($results as $row) {
+if($all=="true") {
+    $statement = $conn->query("SELECT * FROM countries;");
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+} else {
+    $statement = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
   
-    if ($row['name']==$country){
+        if ($row['name']==$country){
             echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
     }
 }
+
+
